9be71baf19e72c45f41a0064d857e6e926400ddb
atena <atenab627@gmail.com>
2022-04-25 08:36:17 -0400

    fdf

--- index.ts
+++ index.ts
@@ -1,1 +1,5 @@
-export * from 'dist/index'
+export * from "./src/auth/index";
+export * from "./src/panel/index";
+export * from "./src/scene/index";
+export * from "./src/product/index";
+export * from "./src/user/index";
--- src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./auth/index";
-export * from "./panel/index";
-export * from "./scene/index";
-export * from "./product/index";
-export * from "./user/index";
