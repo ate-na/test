a372497cc8eb7cac1db96e72bec99e94aac633ef
atena <atenab627@gmail.com>
2022-04-26 01:52:52 -0400

    ff

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export * from './auth/index'
+export * from './panel/index'
+export * from './product/index'
+export * from './user/index'
--- src/panel/index.ts
+++ src/panel/index.ts
@@ -1,2 +1,1 @@
-export * from './panel.schema'
 export * from './users.schema'
